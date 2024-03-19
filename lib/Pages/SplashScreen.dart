913295ea2a06e01765e8ce4faf3a2c6494480772
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2024-03-19 21:09:04 +0530

    add splash screen

--- lib/Pages/SplashScreen.dart
+++ lib/Pages/SplashScreen.dart
@@ -1,18 +1,83 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+import 'package:album/Pages/HomePage.dart';
+import 'package:album/Pages/SigninPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _animationController;
+  Animation<double>? _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 3000),
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_animationController!);
+    _animationController!.forward();
+    Timer(Duration(seconds: 3), () async {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SigninPage()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SigninPage()),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Splash Screen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: FadeTransition(
+        opacity: _animation!,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedOpacity(
+                opacity: 1,
+                duration: Duration(seconds: 1),
+                child: Image.asset(
+                  'Assets/images/splash.jpg',
+                  height: 150,
+                  width: 150,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'memento',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.amber,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
