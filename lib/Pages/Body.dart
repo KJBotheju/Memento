307830afeb7ef21b56d36864a5626b066050f0b9
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2024-03-20 22:04:08 +0530

    fetch images from firebase

--- lib/Pages/Body.dart
+++ lib/Pages/Body.dart
@@ -1,20 +1,41 @@
 import 'package:album/Pages/AddImage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Body extends StatefulWidget {
-  const Body({super.key});
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
 
-  @override
-  State<Body> createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        const Center(
-          child: Text("my album"),
+        StreamBuilder(
+          stream:
+              FirebaseFirestore.instance.collection('PublicImage').snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+              return ListView.builder(
+                padding: EdgeInsets.all(4),
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var imageUrl = snapshot.data!.docs[index]['image_url'];
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              );
+            } else {
+              return Center(child: Text('No images available'));
+            }
+          },
         ),
         Positioned(
           bottom: 16,
